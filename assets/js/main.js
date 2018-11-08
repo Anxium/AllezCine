85d150821390cea3a36ce84ce01d53d1f9c6cb26
BaptisteBourlet <baptiste.bourlet@gmail.com>
2018-11-08 14:31:14 +0100

    formulaire login et register resp

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+let modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+let modale = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
