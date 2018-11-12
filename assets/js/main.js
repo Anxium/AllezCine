955df9bf1c42a6467f726888bc01cdd10eb8201e
BaptisteBourlet <baptiste.bourlet@gmail.com>
2018-11-12 16:08:48 +0100

    newsletter et cookies

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,28 @@ let modale = document.getElementById('id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modale) {
+        modale.style.display = "none";
     }
 }
+//cookie
+
+function toggle(cookie)
+{
+	node = document.getElementById(cookie);
+	if (node.style.visibility=="hidden")
+	{
+		// Contenu caché, le montrer
+		node.style.visibility = "visible";
+		node.style.height = "auto";			// Optionnel rétablir la hauteur
+	}
+	else
+	{
+		// Contenu visible, le cacher
+		node.style.visibility = "hidden";
+		node.style.height = "0";			// Optionnel libérer l'espace
+	}
+}
+$('#boutcook').click(function() {
+  $('#cookie').hide(1000);
+});
